66946e3570cc540104bbe4cc474198de7b0d6821
TulasiJagan49 <jaganmeruga777@gmail.com>
2024-03-02 19:40:31 +0530

    feat(sorting-algorithm): insertion sort

---
+++ algorithms/sortings/insertion_sort.py
@@ -0,0 +1,26 @@
+from sort import Sort
+
+
+class InsertionSort(Sort):
+
+    def __init__(self, list):
+        super().__init__(list)
+
+    def sort(self):
+
+        for i in range(1, self.length):
+            
+            k = self.list[i]
+
+            j = i - 1
+
+            while j >= 0 and self.list[j] > k:
+                self.list[j + 1] = self.list[j]
+                j -= 1
+
+            self.list[j + 1] = k
+
+        return self.list
+
+a_list = [5,2,4,6,1,3]
+print(InsertionSort(a_list).sort())
---
+++ algorithms/sortings/sort.py
@@ -0,0 +1,8 @@
+class Sort(object):
+
+    def __init__(self, list):
+        self.list = list
+        self.length = len(list)
+    
+    def sort(self):
+        raise NotImplementedError
